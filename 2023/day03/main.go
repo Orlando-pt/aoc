610b39d3ee191af15f881bd6137fc4a49ea0504b
Orlando Macedo <orlando.macedo15@gmail.com>
2024-01-01 00:34:19 +0000

    day 3 first solution

--- 2023/day03/main.go
+++ 2023/day03/main.go
@@ -1,22 +1,73 @@
 package day03
 
 import (
-	"fmt"
+	"math"
+	"regexp"
+
+	"github.com/orlando-pt/aoc/2023/utils"
 )
 
-const (
-	LINELENGTH int = 140
-)
+type Coord struct {
+	x int
+	y int
+}
+
+type Number struct {
+    start Coord
+    end Coord
+    val string
+}
 
 func Part1(lines []string) int {
+	ret := 0
+	numbers, specialCharsCoords := parseLines(lines)
 
-    for _, line := range lines {
-        fmt.Println(line)
+    for _, specialCoord := range specialCharsCoords {
+        for _, number := range numbers {
+            if specialCoord.isAdjacentNumber(number) {
+                ret += utils.StrToInt(number.val)
+            }
+        }
     }
-
-    return 0
+	return ret
 }
 
 func Part2(lines []string) int {
-    return 0
+	return 0
 }
+
+func (c Coord) isAdjacent(other Coord) bool {
+    return math.Abs(float64(c.x - other.x)) <= 1 && math.Abs(float64(c.y - other.y)) <= 1
+}
+
+func (c Coord) isAdjacentNumber(number Number) bool {
+    return c.isAdjacent(number.start) || c.isAdjacent(number.end)
+}
+
+func parseLines(lines []string) ([]Number, []Coord) {
+	numbers := make([]Number, 0)
+	specialChars := make([]Coord, 0)
+
+    numbersRegex := regexp.MustCompile(`\d+`)
+    specialRegex := regexp.MustCompile(`[^\d\.]`)
+    for x, line := range lines {
+
+        numbersCoords := numbersRegex.FindAllStringIndex(line, -1)
+        numbersMatches := numbersRegex.FindAllString(line, -1)
+        for i, number := range numbersMatches {
+            numbers = append(numbers, Number{
+                start: Coord{x, numbersCoords[i][0]},
+                end: Coord{x, numbersCoords[i][1]-1},
+                val: number,
+            })
+        }
+
+        specialCharsCoords := specialRegex.FindAllStringIndex(line, -1)
+        for _, match := range specialCharsCoords {
+            specialChars = append(specialChars, Coord{x, match[0]})
+        }
+    }
+
+	return numbers, specialChars
+}
+
---
+++ 2023/test/day03/main_test.go
@@ -0,0 +1,37 @@
+package day03
+
+import (
+	"testing"
+
+	"github.com/orlando-pt/aoc/2023/day03"
+	reader "github.com/orlando-pt/aoc/2023/internal"
+)
+
+func TestPart1(t *testing.T) {
+	input, err := reader.ReadFileLines("01.txt")
+
+	if err != nil {
+		t.Fatalf("Error reading input file: %v\n", err)
+	}
+
+	result := day03.Part1(input)
+
+	if result != 4361 {
+		t.Errorf("Expected 4361, got %d", result)
+	}
+}
+
+// func TestPart2(t *testing.T) {
+// 	input, err := reader.ReadFileLines("01.txt")
+//
+// 	if err != nil {
+// 		t.Fatalf("Error reading input file: %v\n", err)
+// 	}
+//
+// 	result := day03.Part2(input)
+//
+// 	if result != 0 {
+// 		t.Errorf("Expected 2286, got %d", result)
+// 	}
+// }
+
